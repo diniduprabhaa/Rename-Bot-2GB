4a9f664098bca8f0afb40692fd098702e7100a29
diniduprabhaa <dinidusilva902@gmail.com>
2024-06-08 15:28:26 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -4,7 +4,7 @@ from helper.database import jishubotz
 @Client.on_message(filters.private & filters.command(['set_caption', "sc"]))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give The Caption\n\nExample :- `/set_caption 📕Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
+       return await message.reply_text("**Give The Caption\n\nExample :- `/set_caption 📕 Name ➠ : {filename} \n\n🔗 Size ➠ : {filesize} \n\n⏰ Duration ➠ : {duration}`**")
     caption = message.text.split(" ", 1)[1]
     await jishubotz.set_caption(message.from_user.id, caption=caption)
     await message.reply_text("**Your Caption Successfully Added ✅**")
